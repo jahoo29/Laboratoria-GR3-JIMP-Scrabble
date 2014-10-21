375e71a85b2a9e778846523f3eef29dba05d695a
jahoo29 <jahoo29@poczta.onet.pl>
2014-10-21 23:57:52 +0200

    Update tile.hpp

--- tile.hpp
+++ tile.hpp
@@ -1,6 +1,8 @@
 // klasa Tile - obsługuje płytki ktorych gracz nie umiescil na planszy i te ktore
 // juz umiescil ale komp nie zatwierdzil jescze poprawnosci slowa i/lub jego ulozenia
 
+
+
 class Tile {
 	
 	private:
@@ -13,8 +15,12 @@ class Tile {
 		 int value;
 		 int pos_x;
 		 int pos_y;
+		 BITMAP * tile;
+		 bool locked;
 	
 	void create(char* lett,int valu,int x,int y);
+	void lock();
+	void unlock();
 	BITMAP* bmp();
 
 };
