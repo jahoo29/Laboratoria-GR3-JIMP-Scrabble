4d7f171b6dbd429591216053604252c70bbf14b8
jahoo29 <jahoo29@poczta.onet.pl>
2014-10-25 20:05:13 +0200

    Update tile.hpp

--- tile.hpp
+++ tile.hpp
@@ -10,17 +10,24 @@ class Tile {
 	
 	
 	public:
-	
+	// Przechowuje literkę ktora jest nadrukowana na płytce - jest wskaznikiem bo łatwiej jest uzywac wskaznika 
+	// w nektorych funkcjach alegro
 		char* letter;
+	// Wartość punktowa płytki
 		 int value;
+	// pozycje x i y w oknie gry
 		 int pos_x;
 		 int pos_y;
+	// Bitmapa zawierająca obraz płytki
 		 BITMAP * tile;
+	// jesli plytka jest umieszczona na planszy true jesli nie to false
 		 bool locked;
-	
+	// inicjuje płytkę dla danej litery wartości i w danych spółrzędnych
 	void create(char* lett,int valu,int x,int y);
+	//zmiany stanu płytki - bool locked i unclocked
 	void lock();
 	void unlock();
+	// zwraca bitmapę z obrazem płytki
 	BITMAP* bmp();
 
 };
